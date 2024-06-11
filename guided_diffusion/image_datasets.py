8b82bf95ec867b18b8f16411e9865bb8e572e2c8
amir-sabzi <s97.amirsabzi@gmail.com>
2024-06-11 16:06:25 -0400

    (FIX) remove dependency on blobfile

--- guided_diffusion/image_datasets.py
+++ guided_diffusion/image_datasets.py
@@ -2,7 +2,7 @@ import math
 import random
 
 from PIL import Image
-import blobfile as bf
+import os
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
 
@@ -43,7 +43,7 @@ def load_data(
         # Assume classes are the first part of the filename,
         # before an underscore.
         
-        class_names = [bf.basename(path).split("_")[0] for path in all_files]
+        class_names = [os.basename(path).split("_")[0] for path in all_files]
         sorted_classes = {x: i for i, x in enumerate(sorted(set(class_names)))}
         classes = [sorted_classes[x] for x in class_names]
     dataset = ImageDataset(
@@ -70,12 +70,12 @@ def load_data(
 
 def _list_image_files_recursively(data_dir):
     results = []
-    for entry in sorted(bf.listdir(data_dir)):
-        full_path = bf.join(data_dir, entry)
+    for entry in sorted(os.listdir(data_dir)):
+        full_path = os.join(data_dir, entry)
         ext = entry.split(".")[-1]
         if "." in entry and ext.lower() in ["jpg", "jpeg", "png", "gif"]:
             results.append(full_path)
-        elif bf.isdir(full_path):
+        elif os.isdir(full_path):
             results.extend(_list_image_files_recursively(full_path))
     return results
 
@@ -103,7 +103,7 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, idx):
         path = self.local_images[idx]
-        with bf.BlobFile(path, "rb") as f:
+        with open(path, "rb") as f:
             pil_image = Image.open(f)
             pil_image.load()
         pil_image = pil_image.convert("RGB")
