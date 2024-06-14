2f4735155320af879ed24354f92212be67673e07
Amir Sabzi (aurora) <97.amirsabzi@gmail.com>
2024-06-14 15:40:56 -0700

    (FIX) dependencies

--- compute_canada/prepare.sh
+++ compute_canada/prepare.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd ..
 pip install --no-index --upgrade pip
-pip install pytorch=1.7.0 --no-index
-pip install torchvision=0.8.1 --no-index
-pip install numpy=1.19.2 --no-index
-pip install mpi4py --no-index
+pip install torch --no-index
+pip install torchvision --no-index
+pip install numpy --no-index
 pip install matplotlib --no-index
 pip install imageio --no-index
 pip install matplotlib --no-index
